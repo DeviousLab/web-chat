0a0af88ea89d5029fb638557702a2ed68147fc0a
DeviousLab <deviouslab@gmail.com>
2022-07-20 07:36:18 +0400

    Created basic components and router structure
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
+import Home from './components/Home';
+import Login from './components/Login';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
-    <div className="App">
-      Web Chat
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<PrivateRoute />} >
+          <Route path="/" element={<Home />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
   );
 }
 
